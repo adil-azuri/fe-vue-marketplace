19293eed884f1d47bb26049c786d3c83e3738edb
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-11-16 22:52:06 +0700

    Add lucide-vue-next dependency and update sidebar component for improved layout and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
 import Sidebar from "./components/Sidebar.vue";
+import { ref } from "vue";
+
+const minimized = ref(false);
 </script>
 
 <template>
-  <div class="flex h-screen">
-    <!-- Sidebar kiri -->
-    <div class="w-54 bg-gray-100">
-      <Sidebar />
-    </div>
+  <div class="flex h-screen overflow-hidden">
+    <!-- Sidebar -->
+    <Sidebar @toggle="minimized = $event" />
 
-    <!-- Konten kanan -->
-    <div class="flex-1 p-6 overflow-y-auto">
+    <!-- Content -->
+    <div
+      :class="[
+        'flex-1 p-6 overflow-y-auto transition-all duration-300',
+        minimized ? 'ml-16' : 'ml-60',
+      ]"
+    >
       <router-view />
     </div>
   </div>
