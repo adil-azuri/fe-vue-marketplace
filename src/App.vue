e5cf87540464016af0ed200ff9d7512859b84ba1
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-11-16 22:32:12 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <!-- Sidebar kiri -->
+    <div class="w-54 bg-gray-100">
+      <Sidebar />
+    </div>
+
+    <!-- Konten kanan -->
+    <div class="flex-1 p-6 overflow-y-auto">
+      <router-view />
+    </div>
+  </div>
+</template>
